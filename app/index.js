52084e03208affd954419aef3d21d441ce28bdf9
Víctor Andújar <ammavaru@Victors-MacBook-Air.local>
2023-01-21 19:43:26 +0100

    Generate initial population of the game

--- app/index.js
+++ app/index.js
@@ -1,3 +1,12 @@
-import gameInit from "./gameinit/gameinit";
+import boardConstructor from "./board/board.js";
+import gameInit from "./gameinit/gameinit.js";
+import gameTurn from "./gameTurns/gameTurns.js";
 
-console.table(gameInit);
+const rows = 20;
+const columns = 20;
+
+const board = boardConstructor(rows, columns);
+const generateCells = gameInit(board);
+const firstPopulation = gameTurn(generateCells);
+
+console.table(firstPopulation);

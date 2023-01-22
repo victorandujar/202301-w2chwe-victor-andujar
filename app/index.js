38619711b569bb54e20dbbbdd72052466da6be29
victorandujar <victorandujarmurcia@gmail.com>
2023-01-22 16:34:38 +0100

    Fix previous algoritm. Change behaviour of cells

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import boardConstructor from "./board/board.js";
-import algortim from "./gameAlgoritm/gameAlgoritm.js";
+import algoritm from "./gameAlgoritm/gameAlgoritm.js";
 import generateCells from "./generateCells/generateCells.js";
 import gameTurn from "./gameTurns/gameTurns.js";
 
-const rows = 10;
-const columns = 10;
+const rows = 40;
+const columns = 40;
 
 const board = boardConstructor(rows, columns);
 const cells = generateCells(board);
@@ -13,6 +13,6 @@ const firstPopulation = gameTurn(cells);
 console.table(firstPopulation);
 
 setInterval(() => {
-  algortim(cells);
+  algoritm(cells);
   console.table(gameTurn(cells));
 }, 1000);
